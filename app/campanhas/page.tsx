047aba53eae642a99178e13779afcc66009f0f7c
VitorPaes-coder <vpaes3197@gmail.com>
2025-09-18 17:25:02 -0300

    fix: css do card de informação trocado de pasta

--- app/campanhas/page.tsx
+++ app/campanhas/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage(){
             <div className="info-feed">
                 <div className="info-card">
                     <img src="/images/vacina.png" alt="" />
-                    <p></p>
+                    <p>A poliomielite, também conhecida como paralisia infantil, é uma doença contagiosa que pode causar sequelas permanentes e não tem cura. A vacinação é a única forma de prevenção. Durante o período da campanha, todas as crianças menores de 5 anos devem ser levadas às Unidades Básicas de Saúde para receber a dose. Além disso, os pais ou responsáveis podem aproveitar o momento para apresentar a caderneta de vacinação e atualizar outras vacinas em atraso. Vacinar é rápido, seguro e gratuito, e garante proteção para toda a vida.</p>
                 </div>
             </div>
         </main>
