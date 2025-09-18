497085cec5159e2597d4f9b704ba6633b7a34b56
Nicolas <24122316@CFP127ALUNO.LOCAL>
2025-09-18 17:08:23 -0300

    feat(header): header estático finalizado para main branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import { ReactNode } from 'react';
+import { ReactNode } from 'react'
+import './global.css'
+import { Rubik } from 'next/font/google';
 
 export const metadata = {
   title: 'PAS',
   description: 'Painel de Acesso à Saúde (PAS)',
-};
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body style={{ fontFamily: 'Arial, sans-serif', margin: 0, padding: 0 }}>
-        <header style={{ background: '#0070f3', color: 'white', padding: '1rem' }}>
-          <h1>Meu Site Next.js</h1>
-        </header>
+        <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@100;200;300;400;700&display=swap"></link>
+        </head>
+      <body  className="rubik.className">
+        {children}
       </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-export default function HomePage() {
+'use client'
+
+export default function Page() {
     return (
-      <div>
-        <h2>Bem-vindo ao Next.js</h2>
-        <p>Página inicial com TypeScript e JSX funcionando!</p>
-      </div>
+        <header>
+            <div className="logo">
+            <img src="/images/paslogo.png" alt="Logo"/>
+            </div>
+           
+            <nav className="navigation">
+                <a href="">INÍCIO</a>
+                <a href="">MAPA</a>
+            </nav>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
+        </header>
     )
-  }
+}
