25a842b165f901a8ea45ea7ddc6c5abfc62cd01a
Nicolas <nicolas994810523@outlook.com>
2025-09-22 09:34:00 -0300

    fix: colocando children para aparecer as demais rotas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 import './global.css'
 
 export const metadata = {
@@ -5,7 +6,7 @@ export const metadata = {
   description: 'Painel de Acesso à Saúde (PAS)',
 }
 
-export default function RootLayout() {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
         <head>
@@ -24,6 +25,8 @@ export default function RootLayout() {
 
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
         </header>
+        {children}
+
       </body>
     </html>
   )
