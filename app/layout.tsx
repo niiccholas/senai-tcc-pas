92be0ae5dd9df73178fa14c50fb10e780134e816
Nicolas <nicolas994810523@outlook.com>
2025-09-22 08:50:43 -0300

    fix: colocando header no layout para aplicar globalmente

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,29 @@
-import { ReactNode } from 'react'
 import './global.css'
-import { Rubik } from 'next/font/google';
 
 export const metadata = {
   title: 'PAS',
   description: 'Painel de Acesso à Saúde (PAS)',
 }
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout() {
   return (
     <html lang="pt-BR">
         <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@100;200;300;400;700&display=swap"></link>
         </head>
       <body  className="rubik.className">
-        {children}
+        <header>
+            <div className="logo">
+            <img src="/images/paslogo.png" alt="Logo"/>
+            </div>
+           
+            <nav className="navigation">
+                <a href="">INÍCIO</a>
+                <a href="">MAPA</a>
+            </nav>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
+        </header>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
 'use client'
 
 export default function Page() {
-    return (
-        <header>
-            <div className="logo">
-            <img src="/images/paslogo.png" alt="Logo"/>
-            </div>
-           
-            <nav className="navigation">
-                <a href="">INÍCIO</a>
-                <a href="">MAPA</a>
-            </nav>
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
-        </header>
-    )
+    return null
 }
