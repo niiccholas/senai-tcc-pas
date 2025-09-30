3552cb833f091c45b8d2e052d17e28a67ad17910
VITOR PAES RODRIGUES <24122327@dsn127m1249386.cfp127aluno.local>
2025-09-30 09:45:22 -0300

    feat: estrutura básica da página filtro feita

--- app/filtro/page.tsx
+++ app/filtro/page.tsx
@@ -2,5 +2,31 @@
 
 
 import SearchBar from "../components/searchbar/SearchBar"
-import InfoCard from "../components/infocard/InfoCard"
 import "./page.css"
+
+export default function FilterPage(){
+    return(
+        <main>
+            <SearchBar></SearchBar>
+            <h1>SELECIONE O QUE PRECISA:</h1>
+            <div className="filter-card">
+                <div className="section-1">
+                    <div className="specialties">
+
+                    </div>
+                    <div className="24h-service">
+                        
+                    </div>
+                </div>
+                <div className="section-2">
+                    <div className="area-radius">
+
+                    </div>
+                    <div className="public-units">
+                        
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
