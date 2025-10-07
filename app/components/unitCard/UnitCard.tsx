3bbd16453432d8fb3ac3168ce00e5259aea8d8f4
VitorPaes-coder <vpaes3197@gmail.com>
2025-10-07 16:03:41 -0300

    feat: componente de lista de unidades completo

---
+++ app/components/unitCard/UnitCard.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React from 'react'
+import styles from './UnitCard.module.css'
+
+interface UnitCardProps {
+  id: string
+  name: string
+  waitTime?: number
+  onLearnMore?: (unitId: string) => void
+}
+
+export default function UnitCard({ id, name, waitTime, onLearnMore }: UnitCardProps) {
+  const handleLearnMoreClick = () => onLearnMore?.(id)
+
+  return (
+    <div className={styles.unitCard}> 
+      
+        <h3 className={styles.unitName}>{name}</h3>
+
+        <div className={styles.lowerDiv}>
+                <div className={styles.waitContainer}>
+                    <div className={styles.clockIcon}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="black" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clock-icon lucide-clock"><path d="M12 6v6l4 2"/><circle cx="12" cy="12" r="10"/></svg>
+                    </div>
+
+                    <div className={styles.waitBox}>
+                        <span className={styles.waitLabel}>Tempo de espera</span>
+                        <span className={styles.waitValue}>
+                            <b>{waitTime ?? '-'}</b> minutos
+                        </span>
+                    </div>
+                </div>
+
+            <button className={styles.learnMoreBtn} onClick={handleLearnMoreClick} type="button">
+                Saber mais
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export type { UnitCardProps }
--- app/unidades/page.tsx
+++ app/unidades/page.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
-import styles from "./page.module.css"
+'use client';
 
-export default function FilterPage(){
-    return(
-       <main>
-        <div className={styles.unitList}>
-            
+import React, { useEffect, useState } from 'react'
+import styles from './page.module.css'
+import { getUnidades } from '../api/unidade'
+import UnitCard,  {UnitCardProps}  from '../components/unitCard/UnitCard';
+import SearchBar from '../components/searchbar/SearchBar';
+
+export default function FilterPage() {
+  const [unidades, setUnidades] = useState<UnitCardProps[]>([])
+
+  useEffect(() => {
+    async function fetchUnidades() {
+      try {
+        const response = await getUnidades()
+        if (response.status) {
+          const unidadesFormatadas = response.unidadesDeSaude.map((unidade: any) => ({
+            id: String(unidade.id),
+            name: unidade.nome,
+            waitTime: unidade.disponibilidade_24h ? 0 : 30,
+          }));
+          setUnidades(unidadesFormatadas)
+        }
+      } catch (error) {
+        console.error('Erro ao buscar unidades:', error)
+      }
+    }
+
+    fetchUnidades()
+  }, [])
+
+  return (
+    <main className={styles.main}>
+        <div className={styles.unitDiv}>
+            <div className={styles.unitList}>
+                {unidades.map((unidade) => (
+                <UnitCard
+                    key={unidade.id}
+                    id={unidade.id}
+                    name={unidade.name}
+                    waitTime={unidade.waitTime}
+                />
+                ))}
+            </div>
+            <button className={styles.closeUnitList} type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#134879" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+            </button>
         </div>
-        <div className={styles.unitMap}></div>
-       </main>
-    )
-};
+      <div className={styles.unitMap}>
+        <SearchBar></SearchBar>
+      </div>
+    </main>
+  )
+}
