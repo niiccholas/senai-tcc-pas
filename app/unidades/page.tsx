c9f36886b89a0788f7c68c9eac996db801750341
VitorPaes-coder <vpaes3197@gmail.com>
2025-10-07 16:58:29 -0300

    feat: animação ao fechar a lista de unidades

--- app/unidades/page.tsx
+++ app/unidades/page.tsx
@@ -8,6 +8,11 @@ import SearchBar from '../components/searchbar/SearchBar';
 
 export default function FilterPage() {
   const [unidades, setUnidades] = useState<UnitCardProps[]>([])
+  const [isUnitDivVisible, setIsUnitDivVisible] = useState(true)
+
+  const toggleUnitDiv = () => {
+    setIsUnitDivVisible(!isUnitDivVisible)
+  }
 
   useEffect(() => {
     async function fetchUnidades() {
@@ -31,23 +36,30 @@ export default function FilterPage() {
 
   return (
     <main className={styles.main}>
-        <div className={styles.unitDiv}>
-            <div className={styles.unitList}>
-                {unidades.map((unidade) => (
-                <UnitCard
-                    key={unidade.id}
-                    id={unidade.id}
-                    name={unidade.name}
-                    waitTime={unidade.waitTime}
-                />
-                ))}
-            </div>
-            <button className={styles.closeUnitList} type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#134879" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
-            </button>
-        </div>
-      <div className={styles.unitMap}>
+        {isUnitDivVisible && (
+          <div className={styles.unitDiv}>
+              <div className={styles.unitList}>
+                  {unidades.map((unidade) => (
+                  <UnitCard
+                      key={unidade.id}
+                      id={unidade.id}
+                      name={unidade.name}
+                      waitTime={unidade.waitTime}
+                  />
+                  ))}
+              </div>
+              <button className={styles.closeUnitList} onClick={toggleUnitDiv} type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#134879" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+              </button>
+          </div>
+        )}
+      <div className={`${styles.unitMap} ${!isUnitDivVisible ? styles.unitMapFull : ''}`}>
         <SearchBar></SearchBar>
+        {!isUnitDivVisible && (
+          <button className={styles.showUnitList} onClick={toggleUnitDiv} type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#134879" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+          </button>
+        )}
       </div>
     </main>
   )
