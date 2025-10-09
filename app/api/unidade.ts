b591077c8b52b6964b239d4a7b3a755fb9adff94
Your Name <you@example.com>
2025-10-08 23:07:27 -0300

    feat: consumo do endpoint get por id de unidade

--- app/api/unidade.ts
+++ app/api/unidade.ts
@@ -1,7 +1,17 @@
 "use server"
 
 export async function getUnidades() {
-    const url = 'http://10.107.134.34:8080/v1/pas/unidades'
+    const url = 'https://api-tcc-node-js-1.onrender.com/v1/pas/unidades/'
+
+    const response = await fetch(url) // (aguarda) faz uma requisição pra url
+
+    const data = await response.json() // espera o response executar o json com os dados
+
+    return data 
+}
+
+export async function getUnidadesById(id: string) {
+    const url = 'https://api-tcc-node-js-1.onrender.com/v1/pas/unidades/' + id
 
     const response = await fetch(url) // (aguarda) faz uma requisição pra url
 
