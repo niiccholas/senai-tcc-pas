881ab7add4ea2ddcfa0db8fa15156173a5090a38
Your Name <you@example.com>
2025-10-21 23:25:56 -0300

    feat: navegação adicionada ao icone de perfil do menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
-import './global.css'
 import { FiltrosProvider } from './context/FiltroContext'
+import Header from './components/Header'
+import './global.css'
 
 export const metadata = {
   title: 'PAS',
@@ -16,19 +17,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         
       <FiltrosProvider>
       <body className="rubik.className">
-        <header>
-            <div className="logo">
-            <img src="/images/paslogo.png" alt="Logo"/>
-            </div>
-           
-            <nav className="navigation">
-                <a href="/inicio">INÍCIO</a>
-                <a href="/mapa">MAPA</a>
-            </nav>
-
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" className="user-icon"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
-
-        </header>
+        <Header />
 
         {children}
 
